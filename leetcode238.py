13cf1c478a24d067df2d9371144ae0f76d822b83
raphgrey <67438257+raphaelohazurike@users.noreply.github.com>
2023-01-23 03:25:24 -0600

    Create leetcode238.py

---
+++ leetcode238.py
@@ -0,0 +1,24 @@
+#lessons
+# copy as in 'a=b' is a shallow copy referencing the same memory space but just a rename 
+#to deep copy maybe run a loop and append or import some library 
+#also remember nay global array object must be passed in when called
+
+
+nums= [-1,1,0,-3,3] 
+test=[]
+ans=[]
+product=1
+#calc product
+def prod(a,product):
+    for x in a:
+        product=product*x
+    return product
+
+for x in range(0,len(nums)):
+    for y in nums:
+        if y!=nums[x]:
+            test.append(y)
+    ans.append(prod(test,product))
+    test=[]
+
+print(ans)
